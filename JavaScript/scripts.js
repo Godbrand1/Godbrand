20ff7b42cdbcaeed8f8209f44ee89ffb7f2875dd
Godbrand1 <154412048+Godbrand1@users.noreply.github.com>
2023-12-19 23:43:05 -0800

    Add files via upload

---
+++ JavaScript/scripts.js
@@ -0,0 +1,9 @@
+function getUserInput() {
+  // Get the value from the input field
+  var input = document.getElementById('inputLettuce').value;
+  
+  // Do something with the user input (for example, log it to the console)
+  console.log('User input:', input);
+  
+  // You can perform other operations or use this input as needed
+}
