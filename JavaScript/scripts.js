5694f9560c6583b1d853eb6793ce7a33271f795b
Godbrand1 <154412048+Godbrand1@users.noreply.github.com>
2023-12-26 20:15:20 -0800

    Update and rename scripts.js to inventory.js

--- JavaScript/scripts.js
+++
@@ -1,9 +0,0 @@
-function getUserInput() {
-  // Get the value from the input field
-  var input = document.getElementById('inputLettuce').value;
-  
-  // Do something with the user input (for example, log it to the console)
-  console.log('User input:', input);
-  
-  // You can perform other operations or use this input as needed
-}
