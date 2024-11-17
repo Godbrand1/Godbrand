c08d7e4cca27b0181c464a78ad50a1bbae0577c6
Godbrand1 <154412048+Godbrand1@users.noreply.github.com>
2024-11-17 15:07:36 -0800

    Add files via upload

---
+++ aboutme.js
@@ -0,0 +1,11 @@
+// Open About Me Section
+function openAboutMe() {
+    // Changes the overlay's display property to block, making it visible
+    document.getElementById("aboutMeOverlay").style.display = "block";
+}
+
+// Close About Me Section
+function closeAboutMe() {
+    // Changes the overlay's display property to none, hiding it
+    document.getElementById("aboutMeOverlay").style.display = "none";
+}
