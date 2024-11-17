6f4fb5a162a8609d6043ed51d19b0e5b239bcc78
Godbrand1 <154412048+Godbrand1@users.noreply.github.com>
2024-11-17 05:05:08 -0800

    Update script.js

--- script.js
+++ script.js
@@ -100,3 +100,8 @@ function startStopwatch(taskNumber, taskName, duration) {
         });
     });
 }
+window.toggleTheme = function () {
+    const body = document.body;
+    body.classList.toggle('light-theme');
+    console.log("Theme toggled. Current classes:", body.className);
+};
