21afc3607f36259858c84ff49305117bcb9bd382
Godbrand1 <154412048+Godbrand1@users.noreply.github.com>
2024-11-23 17:53:00 -0800

    Add files via upload

--- script/iframe.js
+++ script/iframe.js
@@ -11,8 +11,10 @@ document.getElementById("iframe-container").addEventListener("mouseup", () => {
 // Restore dimensions on page load
 window.addEventListener("load", () => {
     const savedDimensions = JSON.parse(localStorage.getItem("iframeDimensions"));
-    if (savedDimensions) {
-        const iframeContainer = document.getElementById("iframe-container");
+    const iframeContainer = document.getElementById("iframe-container");
+
+    // Check if the iframe is not minimized before restoring dimensions
+    if (savedDimensions && iframeContainer.style.height !== "10%") {
         iframeContainer.style.width = `${savedDimensions.width}px`;
         iframeContainer.style.height = `${savedDimensions.height}px`;
         console.log("Iframe dimensions restored:", savedDimensions);
