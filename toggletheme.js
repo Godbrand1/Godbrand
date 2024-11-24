ee342bf862f26929df845730736806541ffb0b8c
Godbrand1 <154412048+Godbrand1@users.noreply.github.com>
2024-11-23 20:05:05 -0800

    Delete toggletheme.js

--- toggletheme.js
+++
@@ -1,5 +0,0 @@
-window.toggleTheme = function () {
-    const body = document.body;
-    body.classList.toggle('light-theme');
-    console.log("Theme toggled. Current classes:", body.className);
-};
