a0d294def5f2f0d786d745871d6779cb1d735da2
Godbrand1 <154412048+Godbrand1@users.noreply.github.com>
2025-01-06 15:44:26 -0800

    Update Initialization_and_Initial_Setup.js

--- HoTs Stats/scripts/Initialization_and_Initial_Setup.js
+++ HoTs Stats/scripts/Initialization_and_Initial_Setup.js
@@ -19,3 +19,13 @@ document.addEventListener('DOMContentLoaded', () => {
   updateListSelector();
   filterPlayers();
 });
+
+
+const heroCategories = {
+  'Tank': ['Anub\'arak', 'Arthas', 'Blaze', 'Cho', 'Diablo', 'E.T.C.', 'Garrosh', 'Johanna', 'Mal\'Ganis', 'Mei', 'Muradin', 'Stitches', 'Tyrael', 'Varian'],
+  'Bruiser': ['Artanis', 'Chen', 'D.Va', 'Dehaka', 'Deathwing', 'Gazlowe', 'Hogger', 'Imperius', 'Leoric', 'Malthael', 'Ragnaros', 'Rexxar', 'Sonya', 'Thrall', 'Varian', 'Xul', 'Yrel'],
+  'Ranged Assassin': ['Azmodan', 'Cassia', 'Chromie', 'Falstad', 'Fenix', 'Gall', 'Genji', 'Greymane', 'Gul\'dan', 'Hanzo', 'Jaina', 'Junkrat', 'Kael\'thas', 'Kel\'Thuzad', 'Li-Ming', 'Lunara', 'Mephisto', 'Nazeebo', 'Nova', 'Orphea', 'Probius', 'Raynor', 'Sgt. Hammer', 'Sylvanas', 'Tassadar', 'Tracer', 'Tychus', 'Valla', 'Zagara', 'Zul\'jin'],
+  'Melee Assassin': ['Alarak', 'Illidan', 'Kerrigan', 'Maiev', 'Murky', 'Qhira', 'Samuro', 'The Butcher', 'Valeera', 'Varian', 'Zeratul'],
+  'Healer': ['Alexstrasza', 'Ana', 'Anduin', 'Auriel', 'Brightwing', 'Deckard', 'Kharazim', 'Li Li', 'Lt. Morales', 'Lúcio', 'Malfurion', 'Rehgar', 'Stukov', 'Tyrande', 'Uther', 'Whitemane'],
+  'Support': ['Abathur', 'Medivh', 'The Lost Vikings', 'Zarya']
+};
